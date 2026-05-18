83f81c73548ab1f1f3f3f762a323f585238db60e
Edmar <edmarjunioredmar2002@gmail.com>
2026-05-18 20:21:31 -0300

    Criando interface Relatorio

---
+++ src/ueg/com/Relatorio.java
@@ -0,0 +1,22 @@
+package ueg.com;
+
+/**
+ * Interface que define a geração de relatórios para um sistema
+ * de biblioteca.
+ *
+ * <p>
+ *     Implementações desta interface são responsáveis por criar e
+ *     disponibilizar relatórios detalhados sobre o estado ou atividades
+ *     dos recursos geridos por classes como: <strong>empréstimos,
+ *     reservas</strong> ou <strong>inventário de itens.</strong>
+ * </p>
+ * @author Edmar
+ * @since 2026
+ * @version 1.0
+ */
+
+public interface Relatorio
+{
+
+    
+}
