7ba2aab3bb967e3e1a778890ba24da643ebe9192
Edmar <edmarjunioredmar2002@gmail.com>
2026-05-18 19:50:45 -0300

    Iniciando o projeto

---
+++ src/ueg/com/ItemBiblioteca.java
@@ -0,0 +1,22 @@
+package ueg.com;
+
+/**
+ * Interface que define os métodos essenciais para itens que podem ser emprestados e reservados em uma biblioteca.
+ *
+ * <p>
+ *     Implementações desta interface fornecem os mecanismos necessários para manuseio de itens em um sistema de biblioteca,
+ *     permitindo operações de <strong>empréstimo, devolução e reserva</strong>.
+ *     Esta interface foi projetada para ser utilizada como um contrato para classes de recursos de biblioteca, como livros e revistas.
+ * </p>
+ *
+ * @autohor Edmar
+ * @version 1.0
+ * since 2026
+ * @see Livro
+ * @see Revista
+ */
+
+public interface ItemBiblioteca
+{
+
+}
