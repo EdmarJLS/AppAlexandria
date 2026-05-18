cbc3ea6cd4042ff76eedf8ddc604ee3b0e9f0f52
Edmar <edmarjunioredmar2002@gmail.com>
2026-05-18 19:56:57 -0300

    Definição de isDisponivel() e emprestar()

--- src/ueg/com/ItemBiblioteca.java
+++ src/ueg/com/ItemBiblioteca.java
@@ -9,14 +9,26 @@ package ueg.com;
  *     Esta interface foi projetada para ser utilizada como um contrato para classes de recursos de biblioteca, como livros e revistas.
  * </p>
  *
- * @autohor Edmar
+ * @author Edmar
  * @version 1.0
- * since 2026
+ * @since 2026
  * @see Livro
  * @see Revista
  */
 
 public interface ItemBiblioteca
 {
+    /**
+     * Verifica se o item está disponível para empréstimo.
+     *
+     * @return true se o item estiver disponível, false caso contrário.
+     */
+    boolean isDisponivel();
 
+    /**
+     * Marca o item como emprestado. Deve ser chamado quando o item for retirado da biblioteca
+     * por um usuário.
+     */
+
+    void emprestar();
 }
