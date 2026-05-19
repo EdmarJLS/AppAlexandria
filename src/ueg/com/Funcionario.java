f8f8c26ca8766b7eba6918b6ce797262ed3333c9
Edmar <edmarjunioredmar2002@gmail.com>
2026-05-18 22:02:34 -0300

    Criação de classes até o 'getCargo'

--- src/ueg/com/Funcionario.java
+++ src/ueg/com/Funcionario.java
@@ -14,5 +14,26 @@ package ueg.com;
  */
 public class Funcionario extends Pessoa
 {
-    
+    private String cargo;
+
+    /**
+     * Construtor para criar uma nova instância de Funcionário
+     * @param nome - nome do Funcionário
+     * @param cpf - CPF do Funcionário
+     * @param cargo - cargo do Funcionário
+     */
+    public Funcionario(String nome, String cpf, String cargo)
+    {
+        super(nome, cpf);
+        this.cargo = cargo;
+    }
+
+    /**
+     * Retorna o cargo do funcionário
+     * @return cargo
+     */
+    public String getCargo()
+    {
+        return cargo;
+    }
 }
