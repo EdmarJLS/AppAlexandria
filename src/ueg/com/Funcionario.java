fbca6e0bb23c7f409ff8d3f6272950a58ffe2401
Edmar <edmarjunioredmar2002@gmail.com>
2026-05-18 21:35:06 -0300

    Criação da classe 'Funcionário'

---
+++ src/ueg/com/Funcionario.java
@@ -0,0 +1,18 @@
+package ueg.com;
+
+/**
+ * Classe que representa um funcionário no sistema de biblioteca
+ * <p>
+ *     Um funcionário é uma pessoa responsável pela administração
+ *     da biblioteca e possui autoridade para executar ações
+ *     adicionais dentro do sistema.
+ * </p>
+ * @author Edmar
+ * @since 2026
+ * @version 1.0
+ * @see Pessoa
+ */
+public class Funcionario extends Pessoa
+{
+    
+}
