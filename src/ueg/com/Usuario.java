1f41c2c38582b8e6124a40be688c5f2c4b60a776
Edmar <edmarjunioredmar2002@gmail.com>
2026-05-25 19:33:36 -0300

    Criada função getNumeroEmprestimos

--- src/ueg/com/Usuario.java
+++ src/ueg/com/Usuario.java
@@ -5,8 +5,33 @@ package ueg.com;
  * <p>
  *     Um usuário é uma pessoa que pode emprestar itens da biblioteca e tem limite de empréstimos ativos que é ontrolado pelo sistema.
  * </p>
+ *
+ * @author Edmar
+ * @since 2026
+ * @version 1.0
  */
 public class Usuario extends Pessoa
 {
     private int numeroEmprestimos;
+
+    /**
+     * Construtor para criar ua nova instância de Usuário
+     * @param nome
+     * @param cpf
+     */
+    public Usuario(String nome, String cpf)
+    {
+        super(nome, cpf);
+        this.numeroEmprestimos = 0;
+    }
+
+    /**
+     * Retorna a quantidade atual de empréstimos do usuário.
+     * @return numeroEmprestimos
+     */
+    public int getNumeroEmprestimos()
+    {
+        return numeroEmprestimos;
+    }
+    
 }
