74bbff2b94f525b817dd32ee397d77d4d91af90a
Edmar <edmarjunioredmar2002@gmail.com>
2026-05-25 19:26:19 -0300

    Criação da classe 'Usuário'

---
+++ src/ueg/com/Usuario.java
@@ -0,0 +1,12 @@
+package ueg.com;
+
+/**
+ * Representa um usuário no sistema de biblioteca.
+ * <p>
+ *     Um usuário é uma pessoa que pode emprestar itens da biblioteca e tem limite de empréstimos ativos que é ontrolado pelo sistema.
+ * </p>
+ */
+public class Usuario extends Pessoa
+{
+    private int numeroEmprestimos;
+}
