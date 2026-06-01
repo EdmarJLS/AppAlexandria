288f4a5f23cb3910e5b8aa956ae83593e20c50da
Edmar <edmarjunioredmar2002@gmail.com>
2026-06-01 20:14:26 -0300

    Criação da classe Biblioteca

---
+++ src/ueg/com/Biblioteca.java
@@ -0,0 +1,22 @@
+package ueg.com;
+
+/**
+ * Classe que gerencia todas as operações e dados
+ * referentes à biblioteca.
+ * <p>
+ *     Inclui o gerenciamento de itens, usuários e funcionários.
+ *     Fornece métodos para operar a lógica de empréstimos e
+ *     reserva.
+ * </p>
+ * @see ItemBiblioteca
+ * @see Usuario
+ * @see Funcionario
+ * @author Edmar
+ * @since 2026
+ * @version 1.0
+ */
+
+public class Biblioteca implements Relatorio
+{
+
+}
