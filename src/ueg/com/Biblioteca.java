d29668bd98da99bb5cd81803e6576dcfa7cc92ff
Edmar <edmarjunioredmar2002@gmail.com>
2026-06-01 21:57:24 -0300

    Adições de métodos na classe Biblioteca

--- src/ueg/com/Biblioteca.java
+++ src/ueg/com/Biblioteca.java
@@ -63,16 +63,86 @@ public class Biblioteca implements Relatorio
         funcionarios.add(funcionario);
     }
 
+    /**
+     * Método para emprestar um item para um usuário ou funcionário mediante CPF. Se o usuário for um funcionário, não há limite de empréstimos.
+     *
+     * @param titulo - do item
+     * @param cpf - do usuário
+     * @return - Retorna mensagem de sucesso ou falha.
+     */
     public String emprestarItem(String titulo, String cpf)
     {
         for(ItemBiblioteca item: itens)
         {
-            if( ((item instanceof Livro) && ( (Livro) item).equals(titulo) ) ||
-            ( (item instanceof Revista) && ( (Revista) item).getTitulo().equals(titulo) ))
+            // Se ( (item atual é uma instância de Livro) e  titulo do item atual é igual ao titulo informado) OU
+            // Se ( (item atual é uma instância de Revista) e  titulo do item atual é igual ao titulo informado)
+            if( ((item instanceof Livro) && ( (Livro) item).getTitulo().equals(titulo) ) || ( (item instanceof Revista) && ( (Revista) item).getTitulo().equals(titulo) ))
             {
+                if(item.isDisponivel())
+                {
+                    for(Funcionario funcionario : funcionarios)
+                    {
+                        if(funcionario.getCpf().equals(cpf))
+                        {
+                            item.emprestar();
+                            return "Item emprestado com sucesso ao funcionário " + funcionario.getNome();
+                        }
+                    }
+
+                    for(Usuario usuario : usuarios)
+                    {
+                        if(usuario.getCpf().equals(cpf))
+                        {
+                            if(usuario.getNumeroEmprestimos() < 3)
+                            {
+                                item.emprestar();
+                                usuario.incrementaEmprestimos();
+                                return "Item emprestado com sucesso ao usuário " + usuario.getNome();
+                            } else
+                            {
+                                return "Usuário já atingiu o limite de 3 empréstimos.";
+                            }
+                        }
+                    }
+                    return "Usuário não encontrado";
+                }
+            }
+        }
+        return "Nenhum item disponível";
+    }
+
+    /**
+     * Método para devolver um item emprestado para a Biblioteca. Se o usuário não for um funcionário, decrementa o número de empréstimos.
+     * @param titulo - título do item
+     * @param cpf - cpf do usuário
+     * @return - mensagem de sucesso ou falha.
+     */
+    public String devolverItem(String titulo, String cpf)
+    {
+        for(ItemBiblioteca item : itens)
+        {
+            if( (item instanceof Livro) && ( (Livro) item).getTitulo().equals(titulo) || (item instanceof Revista) && ( (Revista) item).getTitulo().equals(titulo))
+            {
+                if(!item.isDisponivel())
+                {
+                    item.devolver();
+
+                    for(Usuario usuario : usuarios)
+                    {
+                        if(usuario.getNome().equals(cpf))
+                        {
+                            usuario.decrementaEmprestimos();
+                        }
+                    }
+                    return "Item devolvido com sucesso!";
+
+                }
+                return "Item não está emprestado!";
 
             }
         }
+        return "Item não encontrado";
+
     }
     @Override
     public void geraRelatorio()
